b1947d3b8868670f3cffd306347617dc0f31c387
deanbirnie <deanicdl@gmail.com>
2023-11-29 09:42:21 +0000

    Task 2 - handling a promise

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,13 @@
+export default function handleResponseFromAPI(promise) {
+  promise
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+
+    .catch((error) => Error(error))
+
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
+}
