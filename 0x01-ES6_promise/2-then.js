ae92e2f97d30f448fc6cdd176a68f5a16dc20d6b
deanbirnie <deanicdl@gmail.com>
2023-11-29 15:04:46 +0000

    Task 2 - attempted again

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,13 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-  promise
-    .then(() => ({
-      status: 200,
-      body: 'success',
-    }))
-
-    .catch((error) => Error(error))
-
-    .finally(() => {
-      console.log('Got a response from the API');
-    });
+  promise.then(() => {
+    console.log('Got a response from the API');
+  }, (error) => error);
 }
