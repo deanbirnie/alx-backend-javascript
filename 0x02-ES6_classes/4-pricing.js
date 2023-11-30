d6b4708086de0f724c932037f6f51320e35c8f0e
deanbirnie <deanicdl@gmail.com>
2023-11-30 19:46:21 +0000

    Task 4 - implementing a static method

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,43 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value === 'number') {
+      this._amount = value;
+    } else {
+      throw new TypeError('Number required');
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (value instanceof Currency) {
+      this._currency = value;
+    } else {
+      throw new TypeError('Must be an instance of Currency');
+    }
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount === 'number' && conversionRate === 'number') {
+      return amount * conversionRate;
+    }
+    throw new TypeError('Amount and Conversion Rate should be of type number');
+  }
+}
